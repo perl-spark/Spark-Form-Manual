5628c2a5c53aadca929f696b3842ef02b3c23a06
Kent Fredric <kentfredric@gmail.com>
2009-06-30 04:00:39 +1200

    further zillifying

--- lib/Spark/Form/Manual.pm
+++ lib/Spark/Form/Manual.pm
@@ -9,10 +9,6 @@ use strict;
 
 =head2 Part 1: L<Getting started|Spark::Form::Manual::Tutorial::GettingStarted>
 
-=head1 AUTHOR
-
-James Laver, C<< <printf(qw(%s at %s.com cpan jameslaver))> >>
-
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-spark-form-manual at rt.cpan.org>, or through
@@ -22,13 +18,6 @@ automatically be notified of progress on your bug as I make changes.
 =head1 SOURCE
 
 Patches are most definitely welcome. Find us on github: L<http://github.com/jjl/Spark-Form-Manual.git>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 James Laver, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
 
 =cut
 
